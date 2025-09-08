c9471dce31494e1214f0dc025ef69a9423112cb2
LazuliKao <LazuliKao233@outlook.com>
2025-09-08 21:42:41 +0800

    Add source hash to kernel source directory naming
    Introduces generate_source_hash function to create a hash based on the kernel source, and updates kernel source directory naming to include this hash. Improves cache management and prevents conflicts when using different sources. Also adds permission handling for overlay and kernel source directories.

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 official_source="SM-S9080_CHN_14_Opensource.zip" # change it with you downloaded file
 build_root=$(pwd)
-kernel_root="$build_root/kernel_source"
 
 container_name="sm8450-kernel-builder"
 
@@ -17,9 +16,11 @@ source "$build_root/scripts/utils/core.sh"
 
 cache_root=$(realpath ${cache_root:-./cache})
 config_hash=$(generate_config_hash)
+source_hash=$(generate_source_hash)
 cache_config_dir="$cache_root/config_${config_hash}"
 cache_platform_dir="$cache_root/sm8450"
 toolchains_root="$cache_platform_dir/toolchains"
+kernel_root="$build_root/kernel_source_$source_hash"
 
 function download_toolchains() {
     mkdir -p "$toolchains_root"
